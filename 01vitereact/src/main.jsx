8bed8eac70709c2671bb39ead690f1058cbe3946
mithunurs17 <mithunursmithun@gamil.com>
2025-11-05 14:59:37 +0530

    counter

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MuApp(){
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+  
+    <anotherElementElement />
+ 
 )
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let counter = 15
+
+  const addValue = () => {
+    console.log("value added", Math.random());
+  }
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>Counter value: {counter}</h2>
+      
+      <button
+      onClick={addValue}>Add value</button>
+      <br />
+      <button>Remove value</button>
+    </>
+  )
+}
+
+export default App
