624ed529dfd95be83a4103537a1078af5e927337
Mithun Urs <151752287+mithunurs17@users.noreply.github.com>
2026-09-07 14:40:53 +0000

    app.js changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Mithun"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Mithun
-        </a>
-      </header>
-    </div>
+  <>
+  <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+  </nav>
+  <div className="container">
+    <h1>Hello {name}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit inventore atque molestias earum quia, id, molestiae unde optio exercitationem consequatur maxime veritatis doloribus soluta, ad eum officiis corporis assumenda voluptates?</p>
+  </div>
+  </>
   );
 }
 
