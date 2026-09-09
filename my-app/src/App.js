695ec83c4bc246c94dc9db80e962e7814c0ce421
Mithun Urs <151752287+mithunurs17@users.noreply.github.com>
2026-09-09 00:40:07 +0000

    Text form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 let name = "Mithun"
 function App() {
   return (
   <>
   
-<Navbar/>
+<Navbar title='TestUtils' aboutText='About us'/>
+<div className='container'>
+  <TextForm/>
+</div>
 
   </>
   );
