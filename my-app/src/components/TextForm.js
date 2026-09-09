0096c7faf2d52c66468039cf2a9d5520dae0ea8a
Mithun Urs <151752287+mithunurs17@users.noreply.github.com>
2026-09-09 01:16:14 +0000

    Preview

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -26,6 +26,8 @@ export default function TextForm(props) {
         <h1>Your text summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
     </div>
     </>
   )
