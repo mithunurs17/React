efbf7ba2ccdf22a00d8416a480b9a9d1c0313055
Mithun Urs <151752287+mithunurs17@users.noreply.github.com>
2026-09-09 01:06:10 +0000

    another container

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -13,7 +13,8 @@ export default function TextForm(props) {
     const [text, setText] = useState('Enter text here');
     // setText("new Text");
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className='mb-3'>
             <textarea className="form-control" value={text} onChange={handleOnChange} id='myBox' rows='8'>
@@ -21,5 +22,10 @@ export default function TextForm(props) {
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
+    <div className='container my-3'>
+        <h1>Your text summary</h1>
+        <p>1111 words and 3343434 characters</p>
+    </div>
+    </>
   )
 }
