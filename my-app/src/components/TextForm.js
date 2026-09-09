b2b4fccea5bc455a9623fc243f62a8f18e419337
Mithun Urs <151752287+mithunurs17@users.noreply.github.com>
2026-09-09 01:20:41 +0000

    lower case

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -6,6 +6,11 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+     const handleLowClick = ()=>{
+        // console.log("Uppercase was clicked"+text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange = ()=>{
         console.log("On Change");
         setText(event.target.value);
@@ -20,7 +25,8 @@ export default function TextForm(props) {
             <textarea className="form-control" value={text} onChange={handleOnChange} id='myBox' rows='8'>
             </textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary" onClick={handleLowClick}>Convert to Lowercase</button>
     </div>
     <div className='container my-3'>
         <h1>Your text summary</h1>
